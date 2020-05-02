921ce6ef7696829a6640f6875286b8adee9ea966
Nurgazy24 <nuroskonbaev24ray@gmail.com>
2020-05-02 21:45:28 +0600

    Add task15Part2 file

---
+++ task15Part2.py
@@ -0,0 +1,11 @@
+seconds1 = int(30)
+minutes1 = int(1)
+hours1 = int(6) 
+seconds2 = int(10) 
+minutes2 = int(2) 
+hours2 = int(6)
+
+total1 = minutes1 * 60 + hours1 * 3600 + seconds1
+
+total2 = minutes2 * 60 + hours2 * 3600 + seconds2
+print(total2 - total1)
